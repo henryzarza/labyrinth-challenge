f161888dd259cfacda37b0dd496334ea9db2fa95
Henry Zarza <henryacz7@gmail.com>
2020-10-16 21:32:40 -0500

    RadioBtn component

---
+++ src/config/constants.ts
@@ -0,0 +1,55 @@
+import apple from '../assets/apple.png';
+import cat from '../assets/cat.png';
+import dog from '../assets/dog.png';
+import flowers from '../assets/flowers.png';
+import mango from '../assets/mango.png';
+import tree from '../assets/tree.png';
+
+export const CHARACTERS = [
+  {
+    src: apple,
+    name: 'apple'
+  },
+  {
+    src: cat,
+    name: 'cat'
+  },
+  {
+    src: dog,
+    name: 'dog'
+  },
+  {
+    src: flowers,
+    name: 'flowers'
+  },
+  {
+    src: mango,
+    name: 'mango'
+  },
+  {
+    src: tree,
+    name: 'tree'
+  }
+];
+
+export const LEVELS = ['beginner', 'intermediate', 'advanced'];
+
+export const MAZES = {
+  BEGINNER: {
+    targetPosition: [6, 9],
+    availableCells: [
+      [1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+      [0, 0, 1, 0, 1, 1, 1, 1, 0, 0],
+      [0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
+      [1, 1, 1, 0, 0, 0, 1, 1, 0, 0],
+      [1, 0, 1, 0, 1, 0, 0, 1, 1, 1],
+      [1, 0, 1, 1, 1, 0, 0, 1, 0, 1],
+      [0, 0, 1, 0, 1, 0, 0, 1, 0, 1],
+      [0, 0, 1, 0, 1, 1, 0, 1, 0, 0],
+      [0, 0, 1, 0, 1, 0, 0, 1, 1, 1]
+    ],
+    startingPosition: [4, 4],
+    moveLimit: 25,
+    cellSize: 30
+  }
+};
